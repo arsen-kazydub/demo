359bd09eba6ce71c221436e23c3153407b69f3ec
webars <arsen.kazydub@gmail.com>
2025-09-06 18:39:59 +0300

    chore(header.php): add meta author tag

--- _shared/header.php
+++ _shared/header.php
@@ -7,6 +7,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Arsen Kazydub">
   <title><?= $page['title'] ?> | Arsen Kazydub</title>
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
