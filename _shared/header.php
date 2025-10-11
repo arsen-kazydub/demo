74f90df409c69803286e8a01b9b59847866d5851
webars <arsen.kazydub@gmail.com>
2025-10-11 06:54:07 +0300

    refactor(header): update stylesheet path

--- _shared/header.php
+++ _shared/header.php
@@ -18,7 +18,7 @@
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
         integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr">
 
-  <link rel="stylesheet" href="../_shared/shared.css">
+  <link rel="stylesheet" href="../../_shared/shared.css">
 
   <?php if (!empty($page['styles'])): ?>
     <?php foreach ($page['styles'] as $style): ?>
